74aed7e24e070435fd890a6b4f016dba5df65b21
markj287 <developer.johnson.mark@gmail.com>
2012-09-05 16:15:58 -0400

    Coppied over a few files from notebud and put it into the JQM mobile template that was provided to us. I also changed to name of the folder

---
+++ Notebud-JQMobile/js/json.js
@@ -0,0 +1,33 @@
+//json object
+//Mark Johnson
+// This will populate our form so we can have easy access to local storage data. Quite frankly am lazy.
+
+var json = {
+	"note1": {
+		"groups": ["Groups:", "Entertainment"],
+		"notetitle": ["Title:","Go See a Movie This Weekend"],
+		"noteinfo": ["Note:","Me and my girlfriend will double date this weekend. Dinner and movie."],
+		"date": ["Date:","June 16, 2012"],
+		"items": ["Number of Items:","4"],
+		"attach": ["Attach a File:",""],
+		"favorite": ["Favorite Note:","Yes"]
+	},
+	"note2": {
+		"groups": ["Groups:", "Grocery"],
+		"notetitle": ["Title:","Grocery Shopping"],
+		"noteinfo": ["Note:","I have to pick up a few items at walmart on thursday."],
+		"date": ["Date:","June 14, 2012"],
+		"items": ["Number of Items:","19"],
+		"attach": ["Attach a File:",""],
+		"favorite": ["Favorite Note:","Yes"]
+	},
+	"note3": {
+		"groups": ["Groups:", "Shopping"],
+		"notetitle": ["Title:","Clothes Shopping"],
+		"noteinfo": ["Note:","I have to buy a fresh outfit for the club this weekend."],
+		"date": ["Date:","June 17, 2012"],
+		"items": ["Number of Items:","5"],
+		"attach": ["Attach a File:",""],
+		"favorite": ["Favorite Note:","Yes"]
+	}
+}
